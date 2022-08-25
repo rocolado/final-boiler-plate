ee8b326877f9c8803ccf98e36b7f1bf4a1b48b03
Rocío Colado <r.colado.chaves@gmail.com>
2022-08-25 15:56:09 +0200

    Added styles in NewTask component

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -2,13 +2,13 @@
   <div class="col-12 | d-flex flex-column | justify-content-center | bg-light | px-4 pt-3 pb-5">
     <div id="form" class="col-12 col-sm-10">
       <div class="text-center">
-        <span>TASK</span>
-        <h1 class="fw-bold">Add a new Task</h1>
-        <h2 class="fs-5">Keep your life organized, prepare for a trip? Start here</h2>
-        <h3 class="fs-6">Today's Date is {{today}}</h3>
+        <span id="task" class="fw-bold">TASK</span>
+        <h1 id="h1" class="fw-bold | mt-2 mb-3">Add a new Task</h1>
+        <h2 class="subtitle">Keep your life organized, prepare for a trip? Start here</h2>
+        <h3 class="subtitle">Today's Date is {{today}}</h3>
       </div>
 
-      <form @submit.prevent="addTask">
+      <form class="my-5" @submit.prevent="addTask">
         <div class="mb-3">
           <input class="form-control shadow-sm | py-2" 
                 type="text" 
@@ -24,7 +24,7 @@
         </div>
 
         <div class="d-flex flex-column | mb-3">
-          <button class="btn btn-success" type="submit">Add</button>
+          <button id="button-add" class="btn" type="submit">Add</button>
         </div>
 
         <div v-if="errorMsg !== ''">
@@ -83,12 +83,44 @@
 </script>
 
 <style>
+  #task {
+    font-size: 12px;
+    color: rgb(14, 159, 110);
+    border-radius: 15px;
+    background-color: rgb(222, 247, 236);
+    padding: 5px 10px;
+  }
+
+  #h1 {
+    font-size: 36px;
+  }
+
+  .subtitle {
+    font-size: 18px;
+  }
+
   #form {
     max-width: 896px;
     margin: auto;
   }
 
-  h1 {
-    font-size: 48px;
+  #button-add {
+    color: white;
+    background-color: rgb(3, 84, 63);
+  }
+
+  #button-add:hover {
+    color: white;
+    background-color: rgb(19, 104, 81);
+  }
+
+  @media (min-width: 768px) {
+    #h1 {
+      font-size: 48px;
+    }
+
+    .subtitle {
+      font-size: 20px;
+    }
   }
 </style>
